523cf7f522675e2b86e7a0410b93b6fb2f7333f5
Marty Zhou <martyzhou@outlook.com>
2016-10-11 23:55:35 +0800

    parse feed

---
+++ test.js
@@ -0,0 +1,26 @@
+var http = require('http-request');
+var fs = require('fs');
+var xml2js = require('xml2js');
+var parser = new xml2js.Parser();
+
+fs.readFile('./feed2.xml', (err, data) => {
+    parser.parseString(data, (err, result) => {
+        console.log(result);
+    });
+});
+/*
+http.get({
+    url: 'http://www.businessmagazin.ro/rss-feed.xml'
+}, (err, res) => {
+    fs.writeFileSync('./feed2.xml', res.buffer.toString());
+});
+*/
+
+/*
+http.get({
+    url: 'http://www.businessmagazin.ro/actualitate/invatamantul-gratuit-nu-e-gratuit-cat-costa-sa-fii-elev-la-o-scoala-de-stat-din-romania-15789601'
+}, (err, res) => {
+    fs.writeFileSync('./test.html', res.buffer.toString());
+    console.log(res.buffer.toString());
+});
+*/
