e167c28efb45dc50f530265ab6f021e872c36680
Marty Zhou <martyzhou@outlook.com>
2016-10-13 00:20:06 +0800

    bbc news

--- test.js
+++ test.js
@@ -3,17 +3,28 @@ var fs = require('fs');
 var xml2js = require('xml2js');
 var parser = new xml2js.Parser();
 
-fs.readFile('./feed2.xml', (err, data) => {
+
+fs.readFile('./feedbbc.xml', (err, data) => {
     parser.parseString(data, (err, result) => {
         console.log(result.rss.channel[0].item[0].link[0]);
 
 	http.get({
 	    url: result.rss.channel[0].item[0].link[0]
 		}, (err, res) => {
-		    fs.writeFileSync('./news1.html', res.buffer.toString());
+		    fs.writeFileSync('./newsbbc.html', res.buffer.toString());
 		});
     });
 });
+
+
+// BBC feed
+/*
+http.get({
+    url: 'http://feeds.bbci.co.uk/news/world/rss.xml'
+}, (err, res) => {
+    fs.writeFileSync('./feedbbc.xml', res.buffer.toString());
+});
+*/
 /*
 http.get({
     url: 'http://www.businessmagazin.ro/rss-feed.xml'
