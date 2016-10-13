a4894ca1b1764f009c449d597a2a07be76b1be35
Marty Zhou <martyzhou@Objectivasoftware.com>
2016-10-13 13:00:17 +0800

    remove comments

--- test.js
+++ test.js
@@ -9,27 +9,58 @@ var parser = new xml2js.Parser();
 fs.readFile('./news1.html', function (err, data) {
     jsdom.env(data.toString(), function (err, window) {
         console.log(window.document.title);
-        console.log(window.document.head);
+        //console.log(window.document.head);
 
-        console.log(window.document.head.children.length);
-        var count = window.document.head.children.length;
-        for (var i = 2; i < count; i++) {
-            window.document.head.children.item(2).remove();
+        //console.log(window.document.head.childNodes);
+        var count = window.document.head.childNodes.length;
+        for (var i = 4; i < count; i++) {
+            window.document.head.childNodes.item(4).remove();
         }
 
-        console.log(window.document.head.children[0]);
-        console.log(window.document.head.children[1]);
-        console.log(window.document.head.children.length);
-        console.log(window.document.title);
+        //console.log(window.document.head.children[0]);
+        //console.log(window.document.head.children[1]);
+        //console.log(window.document.head.children.length);
+        //console.log(window.document.title);
 
         var articalElement = window.document.getElementsByClassName('panel-body').item(0);
         
-        var bodyChildrenCount = window.document.body.children.length;
+        //var bodyChildrenCount = window.document.body.children.length;
+        //for (var i = 0; i < bodyChildrenCount; i++) {
+        //    window.document.body.children.item(0).remove();
+        //}
+
+        var bodyChildrenCount = window.document.body.childNodes.length;
         for (var i = 0; i < bodyChildrenCount; i++) {
-            window.document.body.children.item(0).remove();
+            window.document.body.childNodes.item(0).remove();
         }
 
         window.document.body.appendChild(articalElement);
+
+        var imgs = window.document.getElementsByTagName('img');
+        //http.get({
+        //    url: imgs.item(0).src
+        //}, function (err, res) {
+        //    fs.writeFileSync('./news1img.jpg', res.buffer);
+        //});
+
+        console.log(imgs.item(0).src);
+        imgs.item(0).src = './news1img.jpg';
+
+        var scripts = window.document.getElementsByTagName('script');
+        var scriptLength = scripts.length;
+        for (var i = 0; i < scriptLength; i++) {
+            scripts.item(0).remove();
+        }
+
+        console.log(window.document.body.childNodes);
+        //console.log(window.document.body.children.item(0).childNodes[1]);
+        //console.log(window.document.body.children.item(0).childNodes[2]);
+        //console.log(window.document.body.children.item(0).childNodes[3]);
+        //console.log(window.document.body.children.item(0).childNodes[4]);
+        //console.log(window.document.body.children.item(0).childNodes[5]);
+        //console.log(window.document.body.children.item(0).childNodes[6]);
+        //console.log(window.document.body.children.item(0).childNodes[7]);
+        //console.log(window.document.body.children.item(0).childNodes[8]);
 
         fs.writeFileSync('./news1less.html', serializeDocument(window.document));
     });
