17b6edecda6a87714b80e510124a552d1eddc736
Marty Zhou <martyzhou@Objectivasoftware.com>
2016-10-13 10:42:22 +0800

    extra

--- test.js
+++ test.js
@@ -1,19 +1,28 @@
-var http = require('http-request');
+//var http = require('http-request');
+var jsdom = require("jsdom");
 var fs = require('fs');
 var xml2js = require('xml2js');
 var parser = new xml2js.Parser();
 
-fs.readFile('./feed2.xml', (err, data) => {
-    parser.parseString(data, (err, result) => {
-        console.log(result.rss.channel[0].item[0].link[0]);
 
-	http.get({
-	    url: result.rss.channel[0].item[0].link[0]
-		}, (err, res) => {
-		    fs.writeFileSync('./news1.html', res.buffer.toString());
-		});
+
+fs.readFile('./news1.html', function (err, data) {
+    jsdom.env(data.toString(), function (err, window) {
+        console.log(window.document.body);
     });
 });
+
+//fs.readFile('./feed2.xml', (err, data) => {
+//    parser.parseString(data, (err, result) => {
+//        console.log(result.rss.channel[0].item[0].link[0]);
+
+//	http.get({
+//	    url: result.rss.channel[0].item[0].link[0]
+//		}, (err, res) => {
+//		    fs.writeFileSync('./news1.html', res.buffer.toString());
+//		});
+//    });
+//});
 /*
 http.get({
     url: 'http://www.businessmagazin.ro/rss-feed.xml'
